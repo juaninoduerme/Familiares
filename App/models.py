00525b448521270a1cab48b1e47034faeeda426f
Juani <juanitonoduerme@gmail.com>
2022-04-04 20:09:49 -0300

    Primera versión

---
+++ App/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Familiar(models.Model):
+    idFamiliar = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    fechaDeNacimiento = models.DateField(max_length=30)
+    dni = models.IntegerField(max_length=8)
+    parentezco = models.CharField(max_length=30)
---
+++ App/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from models import Familiar
+from django.template import loader
+
+def mostrar_familiares(self):
+    
+    familiar_uno = Familiar(idFamiliar= '1', nombre= 'Julia', apellido= 'Perez', fechaDeNacimiento= '10/05/1989', dni= '33234687', parentezco= 'Madre')
+    familiar_dos = Familiar(idFamiliar= '2', nombre= 'Martín', apellido= 'Lopez', fechaDeNacimiento= '12/10/1988', dni= '32659784', parentezco= 'Padre')
+    familiar_tres = Familiar(idFamiliar= '3', nombre= 'María', apellido= 'Lopez', fechaDeNacimiento= '20/07/2005', dni= '38256741', parentezco= 'Hija')
+    
+    familiares = {'madre': familiar_uno}
+    familiares = {'padre': familiar_dos}
+    familiares= {'hija': familiar_tres}
+    
+    plantilla = loader.get_template('templateFamiliar.html')
+    documento = plantilla.render(familiares)
+        
+    return HttpResponse(documento)
