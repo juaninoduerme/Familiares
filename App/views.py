291da6b5a09983da8289f3e921f397465c6f726f
Juani <juanitonoduerme@gmail.com>
2022-04-04 20:46:23 -0300

    Arreglos

--- App/views.py
+++ App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Familiar
+from App.models import Familiar
 from django.template import loader
 
 def mostrar_familiares(self):
@@ -8,6 +8,10 @@ def mostrar_familiares(self):
     familiar_uno = Familiar(idFamiliar= '1', nombre= 'Julia', apellido= 'Perez', fechaDeNacimiento= '10/05/1989', dni= '33234687', parentezco= 'Madre')
     familiar_dos = Familiar(idFamiliar= '2', nombre= 'Martín', apellido= 'Lopez', fechaDeNacimiento= '12/10/1988', dni= '32659784', parentezco= 'Padre')
     familiar_tres = Familiar(idFamiliar= '3', nombre= 'María', apellido= 'Lopez', fechaDeNacimiento= '20/07/2005', dni= '38256741', parentezco= 'Hija')
+    
+    familiar_uno.save()
+    familiar_dos.save()
+    familiar_tres.save()
     
     familiares = {'madre': familiar_uno}
     familiares = {'padre': familiar_dos}
--- MVT/settings.py
+++ MVT/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Proyecto/App/template/'],
+        'DIRS': ['Proyecto/App/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
