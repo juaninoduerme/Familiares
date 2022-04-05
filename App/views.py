1c145bd3ead5fb8a069c5d670abd12d8d9478900
Juani <juanitonoduerme@gmail.com>
2022-04-04 21:42:26 -0300

    Funciona Server y HTML

--- App/views.py
+++ App/views.py
@@ -4,18 +4,11 @@ from App.models import Familiar
 from django.template import loader
 
 def mostrar_familiares(self):
-    
-    familiar_uno = Familiar(idFamiliar= '1', nombre= 'Julia', apellido= 'Perez', fechaDeNacimiento= '1989-05-10', dni= '33234687', parentezco= 'Madre')
-    familiar_dos = Familiar(idFamiliar= '2', nombre= 'Martín', apellido= 'Lopez', fechaDeNacimiento= '1988-10-12', dni= '32659784', parentezco= 'Padre')
-    familiar_tres = Familiar(idFamiliar= '3', nombre= 'María', apellido= 'Lopez', fechaDeNacimiento= '2005-07-20', dni= '38256741', parentezco= 'Hija')
-    
-    familiar_uno.save()
-    familiar_dos.save()
-    familiar_tres.save()
-    
-    familiares = {'madre': familiar_uno}
-    familiares = {'padre': familiar_dos}
-    familiares= {'hija': familiar_tres}
+        
+    familiares = dict
+    familiares = {"familiar1": Familiar(idFamiliar= '1', nombre= 'Julia', apellido= 'Perez', fechaDeNacimiento= '1989-05-10', dni= '33234687', parentezco= 'Madre'),
+                  "familiar2": Familiar(idFamiliar= '2', nombre= 'Martín', apellido= 'Lopez', fechaDeNacimiento= '1988-10-12', dni= '32659784', parentezco= 'Padre'),
+                  "familiar3": Familiar(idFamiliar= '3', nombre= 'María', apellido= 'Lopez', fechaDeNacimiento= '2005-07-20', dni= '38256741', parentezco= 'Hija')}
     
     plantilla = loader.get_template('templateFamiliar.html')
     documento = plantilla.render(familiares)
