81a82a080c510b4af0c5b0e9c6ee2046ac7ea7e6
Juani <juanitonoduerme@gmail.com>
2022-04-04 21:05:23 -0300

    El server ya corre

--- App/models.py
+++ App/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Familiar(models.Model):
     idFamiliar = models.IntegerField(primary_key=True)
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    fechaDeNacimiento = models.DateField(max_length=30)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    fechaDeNacimiento = models.DateField()
     dni = models.IntegerField()
     parentezco = models.CharField(max_length=30)
